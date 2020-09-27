a3684550767761512381f6c6f7012b427839b34d
unknown <excerpt.snm@gmail.com>
2020-09-27 22:13:47 +0530

    updated allow and challenge

--- commands/allow.js
+++ commands/allow.js
@@ -6,7 +6,6 @@ module.exports.run = async (bot, message, args) => {
   const holdRole = message.guild.roles.cache.find((r) => r.name === "Onhold");
   const trainer = message.mentions.members.first();
   const trainerName = message.mentions.users.first();
-  let member = message.member;
 
   if (message.channel.id === "710921102033158306") {
     if (message.member.roles.cache.has("747032760748736562")) {
@@ -21,7 +20,7 @@ module.exports.run = async (bot, message, args) => {
         (r) => r.name === "Black Mirror Challenger"
       );
       trainer.send(
-        "Welcome to **Black Mirror Gym** \n This is a **Poison/Psychic Gym** with **Steel Type Pokemon** banned! \n Also **A-Wak, Altaria and Umbreon** are Blacklisted as well! \n Send Request to the Leaders : \n > Kayne98: 0327 8425 5689 \n > Rahul3507: 7531 8026 3356 \n > Shashwizard: 8202 2877 3527 \n > Whitewalker91: 8490 1956 8295 \n DM the Leaders and schedule a time to battle! GL!"
+        "Welcome to **Black Mirror Gym** \n This is a **Poison/Psychic Gym** with **Steel Type Pokemon** banned! \n Also **A-Wak, Altaria and Umbreon** are Blacklisted as well! \n Send Request to the Leaders : \n > Kayne98: 0327 8425 5689 \n > ChampRameshPal: 1371 4791 2705 \n > Shashwizard: 8202 2877 3527 \n > Whitewalker91: 8490 1956 8295 \n DM the Leaders and schedule a time to battle! GL!"
       );
     } else if (message.member.roles.cache.has("745894420125319168")) {
       challengerRole = message.guild.roles.cache.find(
@@ -35,14 +34,14 @@ module.exports.run = async (bot, message, args) => {
         (r) => r.name === "Dark Hail Challenger"
       );
       trainer.send(
-        "Welcome to **Dark Hail Gym** \n This is a **Dark/Ice Gym** with **Counter Users** banned! \n Also **Poliwrath, G-Fisk, Azumarill** are Blacklisted as well! \n **No Mythicals, Legendaries or Shadows** are allowed in this Gym! \n Send Request to the Leaders : \n > agentJC007: 6470 5620 0764 \n > KKinshuKK07: 0006 7284 9976 \n > Prince33246: 6197 5022 9570 \n > TheTaurian22: 5767 2035 1682 \n DM the Leaders and schedule a time to battle! GL!"
+        "Welcome to **Dark Hail Gym** \n This is a **Dark/Ice Gym** with **Counter Users** banned! \n Also **Bastiodon, GFisk, Probopass** are Blacklisted as well! \n **No Mythicals, Legendaries or Shadows** are allowed in this Gym! \n Send Request to the Leaders : \n > agentJC007: 6470 5620 0764 \n > KKinshuKK07: 0006 7284 9976 \n > Prince33246: 6197 5022 9570 \n > TheTaurian22: 5767 2035 1682 \n DM the Leaders and schedule a time to battle! GL!"
       );
     } else if (message.member.roles.cache.has("745896315694612553")) {
       challengerRole = message.guild.roles.cache.find(
         (r) => r.name === "Sacred City Challenger"
       );
       trainer.send(
-        "Welcome to **Sacred City Gym** \n This is a **Steel/Fairy Gym** with **Ground Type Pokemon** banned! \n Also **A-Wak, Vigoroth and Mawile** are Blacklisted as well! \n **No Mythicals, Legendaries or Shadows** are allowed in this Gym! \n Send Request to the Leaders : \n > abhi9890: 0424 6386 0356 \n > Abhishek12197: 0832 6287 7239 \n > kaustubhVK07: 3061 8136 0918 \n > Krish6136: 8669 2747 4200 \n DM the Leaders and schedule a time to battle! GL!"
+        "Welcome to **Sacred City Gym** \n This is a **Steel/Fairy Gym** with **Ground Type Pokemon** banned! \n Also **A-Wak, Vigoroth and Mawile** are Blacklisted as well! \n **No Mythicals, Legendaries or Shadows** are allowed in this Gym! \n Send Request to the Leaders : \n > abhi9890: 0424 6386 0356 \n > Abhishek12197: 0832 6287 7239 \n > darshank06: 6048 3792 9755 \n > Krish6136: 8669 2747 4200 \n DM the Leaders and schedule a time to battle! GL!"
       );
     } else if (message.member.roles.cache.has("745902834402459798")) {
       challengerRole = message.guild.roles.cache.find(
@@ -56,21 +55,21 @@ module.exports.run = async (bot, message, args) => {
         (r) => r.name === "Stronghold Challenger"
       );
       trainer.send(
-        "Welcome to **Stronghold Gym** \n This is a **Normal/Steel Gym** with **Counter User Pokemon** banned! \n Also **Medicham,Perserker,Granbull** are Blacklisted as well! \n **No Mythicals, Legendaries or Shadows** are allowed in this Gym! \n Send Request to the Leaders : \n > 01Alta: 5905 9113 8294 \n > Shri23: 8809 0492 8302 \n > mightymajesty: 6672 8719 0230 \n > Zubzo: 3073 6289 2321 \n DM the Leaders and schedule a time to battle! GL!"
+        "Welcome to **Stronghold Gym** \n This is a **Normal/Steel Gym** with **Counter User Pokemon** banned! \n Also **Raichu,Perserker,Granbull** are Blacklisted as well! \n **No Mythicals, Legendaries or Shadows** are allowed in this Gym! \n Send Request to the Leaders : \n > 01Alta: 5905 9113 8294 \n > Shri23: 8809 0492 8302 \n > mightymajesty: 6672 8719 0230 \n > Zubzo: 3073 6289 2321 \n DM the Leaders and schedule a time to battle! GL!"
       );
     } else if (message.member.roles.cache.has("746194512094822460")) {
       challengerRole = message.guild.roles.cache.find(
         (r) => r.name === "Glacial Jungle Challenger"
       );
       trainer.send(
-        "Welcome to **Glacial Jungle Gym** \n This is a **Ice/Grass Gym** with **Steel Type Pokemon** banned! \n Also **Castform(Sunny), Hypno, Blaziken** are Blacklisted as well! \n **No Mythicals, Legendaries or Shadows** are allowed in this Gym! \n Send Request to the Leaders : \n > Arundrag: 6721 1622 0845 \n > PoketrainerJeet: 9706 5300 9508 \n > SupersaiyaanJoy: 9565 1932 1161 \n > Surajiton: 6321 4156 3006 \n DM the Leaders and schedule a time to battle! GL!"
+        "Welcome to **Glacial Jungle Gym** \n This is a **Ice/Grass Gym** with **Steel Type Pokemon** banned! \n Also **Crustle, Hypno, Blaziken** are Blacklisted as well! \n **No Mythicals, Legendaries or Shadows** are allowed in this Gym! \n Send Request to the Leaders : \n > Arundrag: 6721 1622 0845 \n > PoketrainerJeet: 9706 5300 9508 \n > SupersaiyaanJoy: 9565 1932 1161 \n > Surajiton: 6321 4156 3006 \n DM the Leaders and schedule a time to battle! GL!"
       );
     } else if (message.member.roles.cache.has("746597884446769173")) {
       challengerRole = message.guild.roles.cache.find(
         (r) => r.name === "Lava Ridge Challenger"
       );
       trainer.send(
-        "Welcome to **Lava Ridge Gym** \n This is a **Fire/Ground Gym** with **Water Type Pokemon** banned! \n Also **Dragonair,Drapion,Altaria** are Blacklisted as well! \n **No Mythicals, Legendaries or Shadows** are allowed in this Gym! \n Send Request to the Leaders : \n > AdhuPTY: 0211 4449 2619 \n > GaneshRajesh00: 9754 4131 0486 \n > Legitking2704: 3753 4649 7665 \n > TheDarkPosiedon: 6196 2116 8607 \n DM the Leaders and schedule a time to battle! GL!"
+        "Welcome to **Lava Ridge Gym** \n This is a **Fire/Ground Gym** with **Water Type Pokemon** banned! \n Also **Dragonair,Umbreon,Altaria** are Blacklisted as well! \n **No Mythicals, Legendaries or Shadows** are allowed in this Gym! \n Send Request to the Leaders : \n > AdhuPTY: 0211 4449 2619 \n > GaneshRajesh00: 9754 4131 0486 \n > Legitking2704: 3753 4649 7665 \n > TheDarkPosiedon: 6196 2116 8607 \n DM the Leaders and schedule a time to battle! GL!"
       );
     } else if (message.member.roles.cache.has("746599067034976358")) {
       challengerRole = message.guild.roles.cache.find(
@@ -91,7 +90,7 @@ module.exports.run = async (bot, message, args) => {
         (r) => r.name === "Viridian Challenger"
       );
       trainer.send(
-        "Welcome to **Viridian Gym** \n This is a **Ground/Flying Gym** with **Ice Type Pokemon** banned! \n Also **Azumarill, Cradily and Hypno** are Blacklisted as well! \n **No Mythicals, Legendaries or Shadows** are allowed in Viridian Gym! \n Send Request to the Leaders : \n > Gowthamm432 : 9610 6286 2857 \n > sathyaseven : 1760 7600 6292 \n > DarthDesikan : 5568 9436 1369 \n > Krishna3294 : 0923 8038 9971 \n DM the Leaders and schedule a time to battle! GL!"
+        "Welcome to **Viridian Gym** \n This is a **Ground/Flying Gym** with **Ice Type Pokemon** banned! \n Also **Azumarill, Ferrothorn and Hypno** are Blacklisted as well! \n **No Mythicals, Legendaries or Shadows** are allowed in Viridian Gym! \n Send Request to the Leaders : \n > Gowthamm432 : 9610 6286 2857 \n > sathyaseven : 1760 7600 6292 \n > DarthDesikan : 5568 9436 1369 \n > Krishna3294 : 0923 8038 9971 \n DM the Leaders and schedule a time to battle! GL!"
       );
     } else {
       message.channel.send(
@@ -110,7 +109,7 @@ module.exports.run = async (bot, message, args) => {
     `**${trainerName.username}** has been added to your Gym!`
   );
   trainer.roles.add(challengerRole);
-  member.roles.add(holdRole);
+  trainer.roles.add(holdRole);
 };
 
 module.exports.help = {
--- commands/challenge.js
+++ commands/challenge.js
@@ -41,7 +41,7 @@ module.exports.run = async (bot, message, args) => {
 
           if (reaction.emoji.name === "✅") {
             message.author.send(
-              "Welcome to **Viridian Gym** \n This is a **Ground/Flying Gym** with **Ice Type Pokemon** banned! \n Also **Azumarill, Cradily and Hypno** are Blacklisted as well! \n **No Mythicals, Legendaries or Shadows** are allowed in Viridian Gym! \n Send Request to the Leaders : \n > Gowthamm432 : 9610 6286 2857 \n > sathyaseven : 1760 7600 6292 \n > DarthDesikan : 5568 9436 1369 \n > Krishna3294 : 0923 8038 9971 \n DM the Leaders and schedule a time to battle! GL!"
+              "Welcome to **Viridian Gym** \n This is a **Ground/Flying Gym** with **Ice Type Pokemon** banned! \n Also **Azumarill, Ferrothorn and Hypno** are Blacklisted as well! \n **No Mythicals, Legendaries or Shadows** are allowed in Viridian Gym! \n Send Request to the Leaders : \n > Gowthamm432 : 9610 6286 2857 \n > sathyaseven : 1760 7600 6292 \n > DarthDesikan : 5568 9436 1369 \n > Krishna3294 : 0923 8038 9971 \n DM the Leaders and schedule a time to battle! GL!"
             );
             message.client.channels.cache
               .get("745888605246980156")
@@ -77,7 +77,7 @@ module.exports.run = async (bot, message, args) => {
         return (
           ["✅"].includes(reaction.emoji.name) &&
           (blackmirrorleader.id === "496704628457603072" ||
-            blackmirrorleader.id === "431478273453850634" ||
+            blackmirrorleader.id === "647326966999154688" ||
             blackmirrorleader.id === "653219840416415744" ||
             blackmirrorleader.id === "601273227041308693")
         );
@@ -94,7 +94,7 @@ module.exports.run = async (bot, message, args) => {
 
           if (reaction.emoji.name === "✅") {
             message.author.send(
-              "Welcome to **Black Mirror Gym** \n This is a **Poison/Psychic Gym** with **Steel Type Pokemon** banned! \n Also **A-Wak, Altaria and Umbreon** are Blacklisted as well! \n Send Request to the Leaders : \n > Kayne98: 0327 8425 5689 \n > Rahul3507: 7531 8026 3356 \n > Shashwizard: 8202 2877 3527 \n > Whitewalker91: 8490 1956 8295 \n DM the Leaders and schedule a time to battle! GL!"
+              "Welcome to **Black Mirror Gym** \n This is a **Poison/Psychic Gym** with **Steel Type Pokemon** banned! \n Also **A-Wak, Altaria and Umbreon** are Blacklisted as well! \n Send Request to the Leaders : \n > Kayne98: 0327 8425 5689 \n > ChampRameshPal: 1371 4791 2705 \n > Shashwizard: 8202 2877 3527 \n > Whitewalker91: 8490 1956 8295 \n DM the Leaders and schedule a time to battle! GL!"
             );
             message.client.channels.cache
               .get("745892671582896158")
@@ -129,7 +129,7 @@ module.exports.run = async (bot, message, args) => {
           ["✅"].includes(reaction.emoji.name) &&
           (sacredleader.id === "623026756701650946" ||
             sacredleader.id === "638624295039533056" ||
-            sacredleader.id === "587618049193148436" ||
+            sacredleader.id === "518856827308474399" ||
             sacredleader.id === "690118563511664757")
         );
       };
@@ -145,7 +145,7 @@ module.exports.run = async (bot, message, args) => {
 
           if (reaction.emoji.name === "✅") {
             message.author.send(
-              "Welcome to **Sacred City Gym** \n This is a **Steel/Fairy Gym** with **Ground Type Pokemon** banned! \n Also **A-Wak, Vigoroth and Mawile** are Blacklisted as well! \n **No Mythicals, Legendaries or Shadows** are allowed in this Gym! \n Send Request to the Leaders : \n > abhi9890: 0424 6386 0356 \n > Abhishek12197: 0832 6287 7239 \n > kaustubhVK07: 3061 8136 0918 \n > Krish6136: 8669 2747 4200 \n DM the Leaders and schedule a time to battle! GL!"
+              "Welcome to **Sacred City Gym** \n This is a **Steel/Fairy Gym** with **Ground Type Pokemon** banned! \n Also **A-Wak, Vigoroth and Mawile** are Blacklisted as well! \n **No Mythicals, Legendaries or Shadows** are allowed in this Gym! \n Send Request to the Leaders : \n > abhi9890: 0424 6386 0356 \n > Abhishek12197: 0832 6287 7239 \n > darshank06: 6048 3792 9755 \n > Krish6136: 8669 2747 4200 \n DM the Leaders and schedule a time to battle! GL!"
             );
             message.client.channels.cache
               .get("745896914133975050")
@@ -355,7 +355,7 @@ module.exports.run = async (bot, message, args) => {
 
           if (reaction.emoji.name === "✅") {
             message.author.send(
-              "Welcome to **Dark Hail Gym** \n This is a **Dark/Ice Gym** with **Counter Users** banned! \n Also **Poliwrath, G-Fisk, Azumarill** are Blacklisted as well! \n **No Mythicals, Legendaries or Shadows** are allowed in this Gym! \n Send Request to the Leaders : \n > agentJC007: 6470 5620 0764 \n > KKinshuKK07: 0006 7284 9976 \n > Prince33246: 6197 5022 9570 \n > TheTaurian22: 5767 2035 1682 \n DM the Leaders and schedule a time to battle! GL!"
+              "Welcome to **Dark Hail Gym** \n This is a **Dark/Ice Gym** with **Counter Users** banned! \n Also **Bastiodon, GFisk, Probopass** are Blacklisted as well! \n **No Mythicals, Legendaries or Shadows** are allowed in this Gym! \n Send Request to the Leaders : \n > agentJC007: 6470 5620 0764 \n > KKinshuKK07: 0006 7284 9976 \n > Prince33246: 6197 5022 9570 \n > TheTaurian22: 5767 2035 1682 \n DM the Leaders and schedule a time to battle! GL!"
             );
             message.client.channels.cache
               .get("745932214587490314")
@@ -461,7 +461,7 @@ module.exports.run = async (bot, message, args) => {
 
           if (reaction.emoji.name === "✅") {
             message.author.send(
-              "Welcome to **Glacial Jungle Gym** \n This is a **Ice/Grass Gym** with **Steel Type Pokemon** banned! \n Also **Castform(Sunny), Hypno, Blaziken** are Blacklisted as well! \n **No Mythicals, Legendaries or Shadows** are allowed in this Gym! \n Send Request to the Leaders : \n > Arundrag: 6721 1622 0845 \n > PoketrainerJeet: 9706 5300 9508 \n > SupersaiyaanJoy: 9565 1932 1161 \n > Surajiton: 6321 4156 3006 \n DM the Leaders and schedule a time to battle! GL!"
+              "Welcome to **Glacial Jungle Gym** \n This is a **Ice/Grass Gym** with **Steel Type Pokemon** banned! \n Also **Crustle, Hypno, Blaziken** are Blacklisted as well! \n **No Mythicals, Legendaries or Shadows** are allowed in this Gym! \n Send Request to the Leaders : \n > Arundrag: 6721 1622 0845 \n > PoketrainerJeet: 9706 5300 9508 \n > SupersaiyaanJoy: 9565 1932 1161 \n > Surajiton: 6321 4156 3006 \n DM the Leaders and schedule a time to battle! GL!"
             );
             message.client.channels.cache
               .get("746194971081834497")
@@ -514,7 +514,7 @@ module.exports.run = async (bot, message, args) => {
 
           if (reaction.emoji.name === "✅") {
             message.author.send(
-              "Welcome to **Lava Ridge Gym** \n This is a **Fire/Ground Gym** with **Water Type Pokemon** banned! \n Also **Dragonair,Drapion,Altaria** are Blacklisted as well! \n **No Mythicals, Legendaries or Shadows** are allowed in this Gym! \n Send Request to the Leaders : \n > AdhuPTY: 0211 4449 2619 \n > GaneshRajesh00: 9754 4131 0486 \n > Legitking2704: 3753 4649 7665 \n > TheDarkPosiedon: 6196 2116 8607 \n DM the Leaders and schedule a time to battle! GL!"
+              "Welcome to **Lava Ridge Gym** \n This is a **Fire/Ground Gym** with **Water Type Pokemon** banned! \n Also **Dragonair,Umbreon,Altaria** are Blacklisted as well! \n **No Mythicals, Legendaries or Shadows** are allowed in this Gym! \n Send Request to the Leaders : \n > AdhuPTY: 0211 4449 2619 \n > GaneshRajesh00: 9754 4131 0486 \n > Legitking2704: 3753 4649 7665 \n > TheDarkPosiedon: 6196 2116 8607 \n DM the Leaders and schedule a time to battle! GL!"
             );
             message.client.channels.cache
               .get("746598164559298610")
@@ -620,7 +620,7 @@ module.exports.run = async (bot, message, args) => {
 
           if (reaction.emoji.name === "✅") {
             message.author.send(
-              "Welcome to **Stronghold Gym** \n This is a **Normal/Steel Gym** with **Counter User Pokemon** banned! \n Also **Medicham,Perserker,Granbull** are Blacklisted as well! \n **No Mythicals, Legendaries or Shadows** are allowed in this Gym! \n Send Request to the Leaders : \n > 01Alta: 5905 9113 8294 \n > Shri23: 8809 0492 8302 \n > mightymajesty: 6672 8719 0230 \n > Zubzo: 3073 6289 2321 \n DM the Leaders and schedule a time to battle! GL!"
+              "Welcome to **Stronghold Gym** \n This is a **Normal/Steel Gym** with **Counter User Pokemon** banned! \n Also **Raichu,Perserker,Granbull** are Blacklisted as well! \n **No Mythicals, Legendaries or Shadows** are allowed in this Gym! \n Send Request to the Leaders : \n > 01Alta: 5905 9113 8294 \n > Shri23: 8809 0492 8302 \n > mightymajesty: 6672 8719 0230 \n > Zubzo: 3073 6289 2321 \n DM the Leaders and schedule a time to battle! GL!"
             );
             message.client.channels.cache
               .get("745937838679261274")
