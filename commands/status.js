f91e169872388383c912c8950327516f8c0529bf
Sainayan Mahto <excerpt.snm@gmail.com>
2021-09-04 06:45:22 +0530

    EU Status Command

--- commands/status.js
+++ commands/status.js
@@ -1,28 +1,44 @@
 const Discord = require("discord.js");
 
-const GymData = require("../data/gym");
+const EU = require("../data/EUGymData");
 
 module.exports.run = async (bot, message, args) => {
+  var user = message.author;
+
   const attachment = new Discord.MessageAttachment(
     "./assets/img/logo.png",
     "sample.png"
   );
 
-  const embed = new Discord.MessageEmbed()
-    .setTitle("Tower of Mastery: Gyms Status")
-    .setColor("#ff8b00")
+  if (args[0] === "EU") {
+    const embed = new Discord.MessageEmbed()
+    .setTitle("Tower of Mastery: EU Status")
+    .setColor("#90c9ff")
     .setTimestamp()
     .attachFiles(attachment)
     .setFooter("PvP HQ", "attachment://sample.png");
-  GymData.forEach((i) => {
+    
+  EU.forEach((i) => {
     const Role = message.guild.roles.cache.get(i.gymChallengerRoleID);
     const Members = Role.members.size;
     embed.addField(
-      `${i.gymRoleName} ${i.badgeEmote}`,
+      `${i.gymRoleName} <:EU:870766537580118066>`,
       `${Members} challengers`
     );
   });
+
   message.channel.send(embed);
+
+  } else {
+    const embed = new Discord.MessageEmbed().setDescription(
+      `${bot.users.cache.get(
+        user.id
+      )}, you have to pick EU region at the moment! <:HQ:741349932086198304>`
+    );
+
+  message.channel.send(embed);
+
+  }
 };
 
 module.exports.help = {
